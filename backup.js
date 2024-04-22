b3f979a7c41d6b919a86cfd2fba8e5014b5ab1e1
ahmedtayseer303 <ahmedtayseer303@gmail.com>
2024-04-22 19:21:57 +0200

    updates

--- backup.js
+++
@@ -1,9 +0,0 @@
-// User.findById('65f4859dd7ea54930fd65a65')
-
-// .connect(
-// 'mongodb+srv://ahmedtayseer:D60FZKgWHblCvYDM@cluster0.fadgj0l.mongodb.net/feed?retryWrites=true&w=majority&appName=Cluster0'
-// )
-
-// Stripe Keys
-// public key: pk_test_51OzpTHHAyaBBxzATTl2BoaxEoMNjK2QL8jjZ844j0tP6OylHOZr65aJyx85h6K5TyGO8qZAwpHbcGpNugi1vcbkD00uh7lofsT
-// secret key: sk_test_51OzpTHHAyaBBxzATn0tx7VODOlUW6OKCj5YIleRJvzXPqXlSY0I4TkR1fBbDa3uU4NaJr8kT7DAZOpa9g6yRJ59P00h0a75JIw
